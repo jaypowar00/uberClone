9b80b7f4793e83eb7528d6ba892614f3b7577d15
jaypowar00 <jaypowar00@gmail.com>
2023-03-07 22:33:08 +0530

    [+] added `trip/` & `driver/` url_include in project level urls.py

--- uberClone/urls.py
+++ uberClone/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('user/', include('user.urls')),
+    path('trip/', include('trip.urls')),
+    path('driver/', include('driver.urls')),
     path('admin/', admin.site.urls),
 ]
