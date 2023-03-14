960f98462968180485e63506f59b7c0a803e1bb0
jaypowar00 <jaypowar00@gmail.com>
2023-03-14 13:49:50 +0530

    [+] added /ride/ routes for generate & verify otp

--- ride/urls.py
+++ ride/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('book', views.book_ride),
-    path('cancel', views.cancel_ride)
+    path('cancel', views.cancel_ride),
+    path('generate-otp', views.generate_otp),
+    path('verify-otp', views.verify_otp),
 ]
--- ride/views.py
+++ ride/views.py
@@ -1,12 +1,13 @@
+import math
 import os
-
+import random
+import time
+from datetime import timedelta, datetime
 import requests
-from django.views.decorators.csrf import csrf_exempt
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
-
-from uberClone.settings import idle_drivers
+from uberClone.settings import idle_drivers, ride_otps
 from user.decorators import check_blacklisted_token
 from user.models import User, Ride
 
@@ -14,7 +15,6 @@ from user.models import User, Ride
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 @check_blacklisted_token
-@csrf_exempt
 def book_ride(request):
     user = request.user
     if user.account_type == User.AccountType.DRIVER:
@@ -63,7 +63,8 @@ def book_ride(request):
         "X-RapidAPI-Key": os.getenv('DIRECTION_API_KEY_HEADER', ''),
         "X-RapidAPI-Host": os.getenv('DIRECTION_API_HOST_HEADER', '')
     }
-    response = requests.request("GET", os.getenv('DIRECTION_API_ENDPOINT', 'http://localhost:3000/'), headers=headers, params=querystring).json()
+    response = requests.request("GET", os.getenv('DIRECTION_API_ENDPOINT', 'http://localhost:3000/'), headers=headers,
+                                params=querystring).json()
     if response is None:
         return Response(
             {
@@ -73,8 +74,10 @@ def book_ride(request):
         )
     price = ((response['distance'] / 1000.0) * 105) / vehicle.mileage
     # getting price for driver idle location to customer pickup location travelling
-    querystring = {"origin": f"{idle_drivers[f'{driver_user.id}']['lat']},{idle_drivers[f'{driver_user.id}']['lng']}", "destination": f"{jsn['from_lat']},{jsn['from_lng']}"}
-    response = requests.request("GET", os.getenv('DIRECTION_API_ENDPOINT', 'http://localhost:3000/'), headers=headers, params=querystring).json()
+    querystring = {"origin": f"{idle_drivers[f'{driver_user.id}']['lat']},{idle_drivers[f'{driver_user.id}']['lng']}",
+                   "destination": f"{jsn['from_lat']},{jsn['from_lng']}"}
+    response = requests.request("GET", os.getenv('DIRECTION_API_ENDPOINT', 'http://localhost:3000/'), headers=headers,
+                                params=querystring).json()
     if response is None:
         return Response(
             {
@@ -107,6 +110,103 @@ def book_ride(request):
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 @check_blacklisted_token
-@csrf_exempt
 def cancel_ride(request):
     return None
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+@check_blacklisted_token
+def generate_otp(request):
+    if request.user.account_type == request.user.AccountType.DRIVER:
+        return Response(
+            {
+                'status': False,
+                'message': 'only regular user can generate a new otp'
+            }
+        )
+    jsn = request.body
+    if 'ride_id' not in jsn:
+        return Response(
+            {
+                'status': False,
+                'message': 'Missing ride_id in request'
+            }
+        )
+    ride = Ride.obejcts.filter(id=jsn['ride_id']).first()
+    if ride is None:
+        return Response(
+            {
+                'status': False,
+                'message': 'ride for provided ride_id does not exists'
+            }
+        )
+    digits = "0123456789"
+    otp = ''.join([digits[math.floor(random.random() * 10)] for _ in range(4)])
+    otp_expires = datetime.utcnow() + timedelta(minutes=3)
+    ride_otps[f'{ride.id}'] = {'otp': otp, 'expires': otp_expires}
+    return Response(
+        {
+            'status': True,
+            'otp': f'{otp}'
+        }
+    )
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+@check_blacklisted_token
+def verify_otp(request):
+    if request.user.account_type == request.user.AccountType.REGULAR:
+        return Response(
+            {
+                'status': False,
+                'message': 'otp can be only verified by driver account'
+            }
+        )
+    jsn = request.body
+    if not ('otp' in jsn and 'ride_id' in jsn):
+        return Response(
+            {
+                'status': False,
+                'message': 'missing some parameters in the request (required data: ride_id, otp)'
+            }
+        )
+    ride = Ride.obejcts.filter(id=jsn['ride_id']).first()
+    if ride is None:
+        return Response(
+            {
+                'status': False,
+                'message': 'ride for provided ride_id does not exists'
+            }
+        )
+    current_time = datetime.utcnow()
+    if f'{ride.id}' not in ride_otps:
+        return Response(
+            {
+                'status': False,
+                'message': 'otp is not generated for this ride yet'
+            }
+        )
+    if current_time > ride_otps[f'{ride.id}']['expires']:
+        return Response(
+            {
+                'status': False,
+                'message': 'otp is expired'
+            }
+        )
+    if ride_otps[f'{ride.id}'] != jsn['otp']:
+        return Response(
+            {
+                'status': False,
+                'message': 'incorrect otp'
+            }
+        )
+    ride.otp_verified = True
+    ride.save()
+    return Response(
+        {
+            'status': True,
+            'message': 'otp verified'
+        }
+    )
